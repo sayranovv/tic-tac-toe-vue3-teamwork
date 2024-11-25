f012b6eb27432211c4546cb45dd1edf7f3fd3e94
Vitaliy <razorfathourit@gmail.com>
2024-11-25 12:15:35 +0500

    edit1

--- src/App.vue
+++ src/App.vue
@@ -17,22 +17,4 @@ export default {
 </script>
 
 <style>
-
-body {
-  margin: 0;
-  padding: 0;
-  background-color: #D4DBE2;
-}
-
-h1 {
-  margin-top: 30px;
-  font-family: "Martian Mono", sans-serif;
-  font-size: 1.8rem;
-  color: #7B586B;
-}
-
-#app {
-  text-align: center;
-  font-family: 'Arial', sans-serif;
-}
 </style>
